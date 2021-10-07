19a9667345182d9eb46d0d57c8a15e3f7e64db22
c0027203 <n.smith1@ncl.ac.uk>
2021-10-07 11:32:45 +0100

    Added basic validation to email, first_name, last_name, phone_number, password, confirm_password, pin_key and submit variables from their respective fields in the web-form.

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,23 @@
+import re
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Required, Email, Length, EqualTo, ValidationError
+
 
 
 class RegisterForm(FlaskForm):
-    email = StringField()
-    firstname = StringField()
-    lastname = StringField()
-    phone = StringField()
-    password = PasswordField()
-    confirm_password = PasswordField()
-    pin_key = StringField()
+    email = Email(validators=[Required(), Email])
+    first_name = StringField(validators=[Required(), Email()])
+    last_name = StringField(validators=[Required(), Email()])
+    phone_number = StringField(validators=[Required(), Length(max=11, min=11, message='Phone number must be the correct length of 11 digits.')])
+    password = PasswordField(validators=[Required(), Length(max=15, min=8, message='')])
+    confirm_password = PasswordField(validators=[Required(), Length(max=15, min=8)])
+    pin_key = StringField(validators=[Required(), Length(max=4, min=4)])
     submit = SubmitField()
+
+
+
+
+
+
+
