6b4b180e349f77af2c42f06b9d01cfa9879f2512
c0027203 <n.smith1@ncl.ac.uk>
2021-11-04 14:25:23 +0000

    users\forms.py - Tidied up code and added comments

--- users/forms.py
+++ users/forms.py
@@ -4,6 +4,7 @@ from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import Required, Email, Length, EqualTo, ValidationError
 
 
+# checks to ensure the specified characters are not in the string passed
 def character_check(form,field):
     excluded_chars = "*?"
     for char in field.data:
@@ -12,6 +13,8 @@ def character_check(form,field):
                 f"Character {char} is not allowed")
 
 
+# form for registration of new users with relevant fields for all details required to register a
+# new user and ensure the entered details are validated
 class RegisterForm(FlaskForm):
     email = StringField(validators=[Required(), Email()])
     firstname = StringField(validators=[Required()])
@@ -22,17 +25,21 @@ class RegisterForm(FlaskForm):
     pin_key = StringField(validators=[Required(), Length(min=32, max=32, message='Pin key must be 6 digits long!')])
     submit = SubmitField()
 
+    # validates the phone number passed to ensure it follows the correct format specified below
     def validate_phone(self, phone):
         p = re.compile(r'(\d{4})(-)(\d{3})(-)(\d{4})')
         if not p.match(self.phone.data):
             raise ValidationError("Phone number must be in format of a standard phone number.")
 
+    # validates the password passed to ensure it contains a digit and one uppercase letter.
     def validate_password(self, password):
         p = re.compile(r'(?=.*\d)(?=.*[A-Z])(?=.*[a-z])')
         if not p.match(self.password.data):
             raise ValidationError("Password must contain at least 1 digit and 1 uppercase letter.")
 
 
+# form for login of users, getting and validating their credentials to ensure they
+# are authorised to login
 class LoginForm(FlaskForm):
     email = StringField(validators=[Required(), Email()])
     password = PasswordField(validators=[Required()])
